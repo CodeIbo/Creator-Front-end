d3dd45c61783da610d071f06acbfabf9b0e9eb1e
Ibonom <ibonomgame@gmail.com>
2022-10-15 18:16:57 +0200

    update sass

--- src/app/shared/icon-tab/icon-tab.component.ts
+++ src/app/shared/icon-tab/icon-tab.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { faFacebook,faInstagram,faTiktok,faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
+
+
 @Component({
   selector: 'app-iconTab',
   templateUrl: './icon-tab.component.html',
   styleUrls: ['./icon-tab.component.scss']
 })
-export class IconTabComponent {
+export class IconTabComponent  {
   faFacebook = faFacebook
   faInstagram = faInstagram
   faTiktok = faTiktok
   faLinkedin = faLinkedin
+
 }
