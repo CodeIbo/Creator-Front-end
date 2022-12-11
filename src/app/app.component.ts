168219cffbf02f572d742f6efa3dad22e547a869
Ibonom <ibonomgame@gmail.com>
2022-12-11 18:44:29 +0100

    better UX experience with hamburger menu & scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,30 @@
 import { slideInOut } from './animations/slide-in-out';
-import { Component, HostListener, OnInit } from '@angular/core';
+import {  Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { screenBreakpointsService } from './services/screen-breakpoints.service';
 import { viewManipulation } from './services/view-manipulation.service';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations:[slideInOut]
+  animations:[slideInOut],
 })
 export class AppComponent implements OnInit{
   public innerWidth: number | any;
-  faBars = faBars
- 
+  faBars = faBars;
+  @ViewChild('hamburgermenu') hamburgerMenu: ElementRef | undefined;
+  @ViewChild('sidebar',{ static:false }) sidebar: ElementRef | undefined;
+  
   loadPage = true
   preProdAccess = 'developer1flex'
 
-  constructor(public screenBreakpoints: screenBreakpointsService,public viewManipulation: viewManipulation){
+  constructor(public screenBreakpoints: screenBreakpointsService,public viewManipulation: viewManipulation, public renderer: Renderer2){
     this.innerWidth = window.innerWidth
+
+    this.renderer.listen('window','click',(e:Event)=>{
+      console.log(this.sidebar?.nativeElement)
+    })
   }
 
   getValueFromCookie = (name:string) => {
