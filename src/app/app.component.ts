afa87eb8b18a698a71abc42ae1391ab9802b7cc4
Dawid <ibonomgame@gmail.com>
2024-03-22 15:13:18 +0100

    Prettier update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { slideInOut } from './animations/slide-in-out';
-import {  Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { screenBreakpointsService } from './services/screen-breakpoints.service';
 import { viewManipulation } from './services/view-manipulation.service';
@@ -8,30 +16,25 @@ import { viewManipulation } from './services/view-manipulation.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations:[slideInOut],
+  animations: [slideInOut],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   public innerWidth: number | any;
   faBars = faBars;
   @ViewChild('hamburgermenu') hamburgerMenu: ElementRef | undefined;
-  @ViewChild('sidebar',{ static:false }) sidebar: ElementRef | undefined;
-  
+  @ViewChild('sidebar', { static: false }) sidebar: ElementRef | undefined;
 
-  constructor(public screenBreakpoints: screenBreakpointsService,public viewManipulation: viewManipulation, public renderer: Renderer2){
-    this.innerWidth = window.innerWidth
-
+  constructor(
+    public screenBreakpoints: screenBreakpointsService,
+    public viewManipulation: viewManipulation,
+    public renderer: Renderer2
+  ) {
+    this.innerWidth = window.innerWidth;
   }
-
-
 
   @HostListener('window:resize') onResize() {
     this.innerWidth = window.innerWidth;
-  };
-
-  ngOnInit(): void {
   }
-  
 
+  ngOnInit(): void {}
 }
-
-
