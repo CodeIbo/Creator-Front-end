a59a72316c5b2a3efd9ab75c273da5b0b6712bc8
Dawid <ibonomgame@gmail.com>
2024-03-22 15:10:49 +0100

    New scroll service  (resets view after switch page)

--- src/app/services/scroll.service.ts
+++ src/app/services/scroll.service.ts
@@ -1,19 +1,28 @@
-import { Injectable } from "@angular/core";
-
-
+import { Injectable } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class scrollService {
-    lastElement: string | any;
-    
-    scroll = (el:string) =>{
-        setTimeout(() =>{
-            const element = document.querySelector(el)
-            if(element !== null){
-                this.lastElement = el
-                element.scrollIntoView({behavior: "smooth",block:"start"})
-            }
-        },100)
-    }
-}
+  lastElement: string | any;
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.resetScrollPosition();
+      });
+  }
 
+  scroll = (el: string) => {
+    setTimeout(() => {
+      const element = document.querySelector(el);
+      if (element !== null) {
+        this.lastElement = el;
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }, 100);
+  };
+  private resetScrollPosition = () => {
+    window.scrollTo(0, 0);
+  };
+}
