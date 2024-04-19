47d6ed05386a4100cdc43cb7abc4c0d3258b9017
Dawid <ibonomgame@gmail.com>
2024-04-19 10:24:22 +0200

    Menu Model from api

---
+++ src/app/models/Api/menu.model.ts
@@ -0,0 +1,10 @@
+export interface MenuAttributes {
+  id: string;
+  url_id: string;
+  menu_order: number;
+  label: string;
+  parent_id: string | null;
+  created_at: string;
+  url: string;
+  page_category: string;
+}
