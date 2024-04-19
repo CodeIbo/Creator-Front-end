cdd5c0b6038fb5f9af5958685c361080ffc10846
Dawid <ibonomgame@gmail.com>
2024-04-19 10:15:57 +0200

    New Slider Component

---
+++ src/app/components/slider/slider.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.scss'],
+})
+export class SliderComponent implements OnInit {
+  configSlider = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 1000,
+    dots: true,
+    infinite: true,
+  };
+  @Input() galleryItems: any = [
+    {
+      img: '',
+      alt: '',
+      txt: '',
+    },
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
