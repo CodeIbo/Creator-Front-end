e3fa130d0ee0ecb8bfafe1e162135bc66f8386ff
Dawid <ibonomgame@gmail.com>
2024-04-19 10:09:51 +0200

    Slide in-out to be faster

--- src/app/animations/slide-in-out.ts
+++ src/app/animations/slide-in-out.ts
@@ -7,6 +7,6 @@ export const slideInOut = trigger('slideInOut',[
         transform:'translate(0)',
     })
     ),
-    transition('void => *',[style({transform:'translate(-100%)'}),animate('1.25s ease')]),
-    transition('* => void',[animate('1s ease-out'),style({transform:'translateX(-100%)'})])
+    transition('void => *',[style({transform:'translate(-100%)'}),animate('0.45s ease')]),
+    transition('* => void',[animate('0.45s ease-out'),style({transform:'translateX(-100%)'})])
   ]); 
