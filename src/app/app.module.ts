47bd65cc254559d8315fa8045fdfa1b5e432cbd3
Dawid <ibonomgame@gmail.com>
2024-06-14 12:22:11 +0200

    Move footer to component/internal folder & sort imports in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms';
 import { firstValueFrom } from 'rxjs';
+import { NgHttpCachingConfig, NgHttpCachingModule } from 'ng-http-caching';
 
 import { AppComponent } from './app.component';
+
+//Components
 import { SMIconsComponent } from '@components/internal/sm-icons/icon-tab.component';
 import { NavBarComponent } from '@components/internal/side-nav/side-nav.component';
+import { FooterComponent } from '@components/internal/footer/footer.component';
 import { MenuComponent } from '@components/internal/menu/menu.component';
+
+//Modules
 import { AppRoutingModule } from '@modules/app-routing.module';
 import { CustomTagsModule } from '@modules/custom-tags.module';
+
+//Directives
 import { DynamicComponentLoaderDirective } from '@directives/dynamic-component-loader.directive';
 import { MutationObserverDirective } from '@directives/mutation-observer.directive';
+
+//Pages
 import { HomePageComponent } from '@client/pages/home/home-page.component';
 import { ErrorNotFoundComponent } from '@pages/error-not-found/error-not-found.component';
 import { CustomPageComponent } from '@pages/custom-page/custom-page.component';
 import { BlogComponent } from '@pages/blog/blog.component';
 import { ArticleComponent } from '@pages/blog/article/article.component';
+
+//Filters
 import { FilterPipe } from '@filters/filter.pipe';
 import { SanitizeHtmlPipe } from '@filters/sanitizeHtml.pipe';
 import { SortByDatePipe } from '@filters/sortByDate.pipe';
+
+//Services
 import { SettingsService } from '@services/settings.service';
-import { NgHttpCachingConfig, NgHttpCachingModule } from 'ng-http-caching';
-import { FooterComponent } from './pages/footer/footer.component';
 
 export function initializeApp(settingsService: SettingsService) {
   return () => firstValueFrom(settingsService.fetchSettings());
